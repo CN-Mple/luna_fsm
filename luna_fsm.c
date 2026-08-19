8c6996099c2b669c934a268af6ad0dfa40caa202
lunar <2434717958@qq.com>
2026-08-19 12:52:30 +0800

    fix(func): rename sig, new ctor func, support action single change with evt.

--- luna_fsm.c
+++ luna_fsm.c
@@ -1,34 +1,37 @@
 /* luna_fsm.c */
 #include "luna_fsm.h"
 
-static const struct luna_fsm_evt luna_internal_sig[] = {
-	{.sig = SIG_EMPTY},
-	{.sig = SIG_ENTER},
-	{.sig = SIG_EXIT},
-	{.sig = SIG_INIT},
-	{.sig = SIG_INTERRUPT},
+static const struct luna_fsm_evt luna_fsm_reserve_sigs[] = {
+	{.sig = EMPTY_SIG},
+	{.sig = ENTER_SIG},
+	{.sig = EXIT_SIG},
+	{.sig = INIT_SIG},
 };
 
-void luna_fsm_init(struct luna_fsm *me)
+void luna_fsm_ctor(struct luna_fsm *me, luna_fsm_handler_t handler)
+{
+	me->handler = handler;
+}
+
+void luna_fsm_init(struct luna_fsm *me, struct luna_fsm_evt const *evt)
 {
 	LUNA_FSM_ASSERT(me);
 	LUNA_FSM_ASSERT(me->handler);
 
-	(void)(*me->handler)(me, &luna_internal_sig[SIG_INIT]);
-	(void)(*me->handler)(me, &luna_internal_sig[SIG_ENTER]);
+	(void)(*me->handler)(me, evt);
+	(void)(*me->handler)(me, &luna_fsm_reserve_sigs[ENTER_SIG]);
 }
 
-void luna_fsm_dispatch(struct luna_fsm *me, const struct luna_fsm_evt *evt)
+void luna_fsm_dispatch(struct luna_fsm *me, struct luna_fsm_evt const *evt)
 {
 	LUNA_FSM_ASSERT(me);
 	LUNA_FSM_ASSERT(evt);
+	LUNA_FSM_ASSERT(me->handler != 0);
 
 	luna_fsm_handler_t handler = me->handler;
-	LUNA_FSM_ASSERT(handler != NULL);
-
-	uint32_t rt = handler(me, evt);
-	if (TRANSFER == rt) {
-		(void)handler(me, &luna_internal_sig[SIG_EXIT]);
-		(void)(*me->handler)(me, &luna_internal_sig[SIG_ENTER]);
+	uint32_t rt = (*handler)(me, evt);
+	if (RET_TRANSFER == rt) {
+		(void)(*handler)(me, &luna_fsm_reserve_sigs[EXIT_SIG]);
+		(void)(*me->handler)(me, &luna_fsm_reserve_sigs[ENTER_SIG]);
 	}
 }
--- luna_fsm.h
+++ luna_fsm.h
@@ -8,14 +8,14 @@
 #define LUNA_FSM_ASSERT(expr) ((void)0)
 #endif
 
-#define HANDLED		(uint32_t)(0)
-#define IGNORED		(uint32_t)(1)
-#define TRANSFER	(uint32_t)(2)
+#define RET_HANDLED		(uint32_t)(0)
+#define RET_IGNORED		(uint32_t)(1)
+#define RET_TRANSFER		(uint32_t)(2)
 
-#define HAND()		HANDLED
-#define IGNO()		IGNORED
+#define HAND()		RET_HANDLED
+#define IGNO()		RET_IGNORED
 #define TRAN(t) \
-			me->handler = t, TRANSFER
+			(((struct luna_fsm *)me)->handler = (luna_fsm_handler_t)(t), RET_TRANSFER)
 
 struct luna_fsm_evt {
 	uint32_t sig;
@@ -25,21 +25,21 @@ struct luna_fsm;
 
 typedef uint32_t (*luna_fsm_handler_t)(struct luna_fsm *fsm, const struct luna_fsm_evt *evt);
 
+// Based on the current status and the received events, perform actions according to predefined rules and switch the status.
 struct luna_fsm {
 	luna_fsm_handler_t handler;
 };
 
 enum {
-	SIG_EMPTY = 0,
-	SIG_ENTER,
-	SIG_EXIT,
-	SIG_INIT,
-	SIG_INTERRUPT,
-	SIG_RESERVE_SIG_NUM,
-	CUSTOM_SIG = SIG_RESERVE_SIG_NUM,
+	EMPTY_SIG = 0,
+	ENTER_SIG,
+	EXIT_SIG,
+	INIT_SIG,
+	USER_SIG,
 };
 
-void luna_fsm_init(struct luna_fsm *me);
+void luna_fsm_ctor(struct luna_fsm *me, luna_fsm_handler_t handler);
+void luna_fsm_init(struct luna_fsm *me, struct luna_fsm_evt const *evt);
 void luna_fsm_dispatch(struct luna_fsm *me, const struct luna_fsm_evt *evt);
 
 #endif
